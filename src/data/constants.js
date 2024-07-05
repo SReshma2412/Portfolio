1a7dc795dbc9f3b696400148a7f39a5e51388ea8
Your Name~ <your email>
2024-07-05 17:17:05 +0530

    Files Added

---
+++ src/data/constants.js
@@ -0,0 +1,177 @@
+export const Bio = {
+    name: "Reshma S",
+    roles: [
+      "MERN FullStack Developer",
+      "Software Engineer",
+    ],
+    description:
+      "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+    github: "https://github.com/SReshma2412",
+    resume:
+     "https://drive.google.com/file/d/18Ehai95WVyAWp1pbvLi7QOmUK4KkpblM/view?usp=sharing" ,
+    linkedin:"https://www.linkedin.com/feed/?trk=guest_homepage-basic_google-one-tap-submit" ,
+    twitter: "https://twitter.com/ReshmaS1787100",
+    insta: "https://www.instagram.com/resh_kutty76/",
+    facebook:"https://www.facebook.com/profile.php?id=100087617051488" ,
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+    
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+       
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+     
+       
+        {
+          name: "MongoDB",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        },
+       
+      ],
+    },
+    {
+      title: "Android,databse",
+      skills: [
+        {
+          name: "Java",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        {
+          name: "JDBC",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+       
+        {
+          name: "SQL",
+          image:
+            "https://www.vectorlogo.zone/logos/mysql/mysql-ar21.svg",
+        },
+        {
+          name: "Git",
+          image:
+            "https://www.vectorlogo.zone/logos/git-scm/git-scm-ar21.svg",
+        },
+       
+       
+       
+      ],
+    },
+   
+       
+
+  ];
+  
+  export const experiences = [
+     
+    {
+      id: 0,
+      role: "MERN FullStack Intern",
+      company: "Ethunus",
+      date: "August 2023 -december 2023",
+      desc: "Working on the frontend of the web application using ReactJS",
+      skills: [
+        
+        "ReactJS",
+        "NodeJs",
+        "HTML",
+        "CSS",
+        "Bootstrap",
+        "JavaScript",
+        "MongoDB"
+      ],
+     
+    },
+   
+   
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      school: "Vellore Institute of Technology,Vellore",
+      date: " 2021 - 2025",
+      grade: "8.61 CGPA",
+      desc: "I am currently pursuing a BTech Information Technology at Vellore Institute of Technology,Vellore. I have completed 6 semesters and have a CGPA of 8.61. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+      degree: "Bachelor of Technology - BTech Infromation Technology",
+    },
+    {
+      id: 1,
+      school: "Government Modern Higher Secondary School,Thirumullaivasal",
+      date: " 2020 -2021",
+      grade: "91.4%",
+      desc: "I completed my class 12 high school education at Government Higher Secondary School,Thirumullaivasal  where I studied Maths-Biology.",
+      degree: "Maths-Biology",
+    },
+    {
+      id: 2,
+      school: "Government Modern Higher Secondary School,Thirumullaivasal",
+      date: "2018 - Apr 2019",
+      grade: "91%",
+      desc: "I completed my class 10 education at Government Higher Secondary School,Thirumullaivasal,  where I studied Maths-Biology",
+      degree: "Maths-Biology",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Bus Reservation System Project using Java and JDBC",
+      description:
+        " Designed and managed a MySQL database schema with tables for buses, routes, schedules, and reservations, ensuring data integrity and efficient storage.",
+
+    },
+    {
+      id: 2,
+      title: "Banking System Project using Java and JDBC",
+      description:
+        "Designed and managed a MySQL database schema with tables for users and accounts, ensuring data integrity and efficient storage ",
+
+    },
+    {
+      id: 3,
+      title: "Hospital Management System Project using Java and JDBC",
+      description:
+        "Designed and managed a MySQL database schema with tables for patients, doctors, and appointments, ensuring data integrity and efficient storage.",
+
+    },
+    
+   
+  ];
+  
